f3487c834b54949aeeaa4450e16434f11254cef5
PurpleSavage <jzuritapalomino@gmail.com>
2024-10-12 09:27:54 -0500

    arreglando el voice chanel

--- src/features/landing/components/Header.tsx
+++ src/features/landing/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 import Link from 'next/link';
 export default function Header() {
   return (
-    <header className="flex items-center border-b border-slate-700 py-4 px-4 w-full">
+    <header className="flex items-center border-b border-slate-700 py-4 px-4 w-full ">
         <Link href="/" className="flex gap-4 items-center cursor-pointer">
           <Image
             width={30}
