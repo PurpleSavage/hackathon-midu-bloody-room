9e9208cc49ca3eec1404826e3410a101f685cef2
Ariano2700 <kawuai123loco123@gmail.com>
2024-10-22 13:57:02 -0500

    envoriments varibles add in config firebase

--- src/service/config.ts
+++ src/service/config.ts
@@ -3,16 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey:process.env.APIKEY_FIREBASE,
-  authDomain:process.env.AUTH_DOMAIN,
-  projectId:process.env.PROJECT_ID,
-  storageBucket:process.env.STORAGE_BUCKET,
-  messagingSenderId:process.env.MESSASGING_SENDER_ID,
-  measurementId:process.env.MESSURE_ID
+  apiKey: process.env.NEXT_PUBLIC_APIKEY_FIREBASE,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSASGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_MESSURE_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-export {app, db, auth};
+export { app, db, auth };
